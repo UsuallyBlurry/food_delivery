9aed9c3c9417bea11520ac703950495e6e7f5f0b
UsuallyBlurry <136008901+UsuallyBlurry@users.noreply.github.com>
2023-09-18 17:23:20 +0100

    style issue

--- data.js
+++ data.js
@@ -1,8 +1,104 @@
-let products = [
-  {
-    id: 578,
-    name: 'chicken tajine',
-    description: 'some description on chicken tajine',
-  },
 
+const driveria = [
+  {
+    name: 'dar naji',
+    address: 'avenue de france',
+    menu: [
+      {
+      id: '450',
+      name: 'chi haja bnina',
+      price: 55,
+      img: "",
+      tags: ['chi', 'haja'],
+    },
+    {
+      id: '451',
+      name: 'chi haja bnina',
+      price: 55,
+      img: "",
+      tags: ['chi', 'haja'],
+    },
+    {
+      id: '452',
+      name: 'chi haja bnina',
+      price: 55,
+      tags: ['chi', 'haja'],
+    },
+    {
+      id: '453',
+      name: 'chi haja bnina',
+      price: 55,
+      img: "",
+      tags: ['chi', 'haja'],
+    }
+  ],
+  },
+  {
+    name: 'dar cherkaoui',
+    address: 'avenue oqba',
+    menu: [{
+      id: '230',
+      name: 'chi haja bnina chwia',
+      price: 40,
+      img: "",
+      tags: ['chi', 'haja', 'chwia'],
+    },
+    {
+      id: '231',
+      name: 'chi haja bnina',
+      price: 55,
+      img: "",
+      tags: ['chi', 'haja'],
+    },
+    {
+      id: '232',
+      name: 'chi haja bnina',
+      price: 55,
+      img: "",
+      tags: ['chi', 'haja'],
+    },
+    {
+      id: '233',
+      name: 'chi haja bnina',
+      price: 55,
+      img: "",
+      tags: ['chi', 'haja'],
+    },
+  ],
+  },
+  {
+    name: 'zayyane',
+    address: 'avenue ennakhil',
+    menu: [
+      {
+      id: '110',
+      name: 'chi haja',
+      price: 30,
+      img: "https://images.unsplash.com/photo-1541518763669-27fef04b14ea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bW9yb2NjYW4lMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+      tags: ['chi', 'haja'],
+      },
+      {
+        id: '111',
+      name: 'chi haja bnina',
+      price: 55,
+      img: "https://images.unsplash.com/photo-1574810220185-df93df6f81e5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bW9yb2NjYW4lMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+      tags: ['chi', 'haja'],
+      },
+      {
+        id: '112',
+      name: 'chi haja bnina',
+      price: 55,
+      img: "https://images.unsplash.com/photo-1661083098412-054431ab7112?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bW9yb2NjYW4lMjBmb29kJTIwdGFqaW5lfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+      tags: ['chi', 'haja'],
+      },
+      {
+        id: '453',
+      name: 'chi haja bnina',
+      price: 55,
+      img: "",
+      tags: ['chi', 'haja'],
+      },
+  ],
+  },
 ]
+
